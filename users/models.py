868bf0c3f15ee1ef6d9d7940216bacce17a54952
Areeb <areebshahid123@gmail.com>
2023-09-15 12:57:36 +0500

    userprofile

--- users/models.py
+++ users/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django .contrib.auth.models import User
-
+from PIL import Image
+import os
 
 # Create your models here.
 class Profile(models.Model):
@@ -9,3 +10,20 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username} Profile '
+
+    def save(self):
+        super().save()
+
+        img = Image.open(self.image.path)
+
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
+
+    def delete(self, *args, **kwargs):
+        if self.image.name != 'default.jpg':
+            if os.path.isfile(self.image.path):
+                os.remove(self.image.path)
+
+        super().delete(*args, **kwargs)
