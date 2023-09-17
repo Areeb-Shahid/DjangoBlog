6b635e1e8af331f809ba2a817f1320c69ae84859
Areeb <areebshahid123@gmail.com>
2023-09-17 21:14:27 +0500

    userprofile

--- users/models.py
+++ users/models.py
@@ -3,6 +3,7 @@ from django .contrib.auth.models import User
 from PIL import Image
 import os
 
+
 # Create your models here.
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
