7097d3364f5113fb50a6f0b82873b8e93e6d34c3
Areeb <areebshahid123@gmail.com>
2023-09-18 23:41:15 +0500

    blog post create/delete/update

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,14 @@
+from abc import ABC
+
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView
+)
 from .models import Post
 
 
@@ -20,6 +29,41 @@ class PostListView(ListView):
 class PostDetailView(DetailView):
     model = Post
 
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        else:
+            False
+
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView ):
+    model = Post
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        else:
+            False
 
 
 def about(request):
