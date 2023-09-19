f7772c5942d5c43787e293e4594aafa8179390fd
Areeb <areebshahid123@gmail.com>
2023-09-19 16:50:05 +0500

    pagination

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,6 @@
-from abc import ABC
-
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.models import User
 from django.views.generic import (
     ListView,
     DetailView,
@@ -21,9 +20,21 @@ def home(request):
 
 class PostListView(ListView):
     model = Post
-    template_name = 'blog/home.html'
+    template_name = 'blog/home.html'  # <app>/<model>_<viewtype>.html
     context_object_name = 'posts'
     ordering = ['-date_posted']
+    paginate_by = 5
+
+
+class UserPostListView(ListView):
+    model = Post
+    template_name = 'blog/user_posts.html'  # <app>/<model>_<viewtype>.html
+    context_object_name = 'posts'
+    paginate_by = 5
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Post.objects.filter(author=user).order_by('-date_posted')
 
 
 class PostDetailView(DetailView):
@@ -51,11 +62,10 @@ class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
         post = self.get_object()
         if self.request.user == post.author:
             return True
-        else:
-            False
+        return False
 
 
-class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView ):
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Post
     success_url = '/'
 
@@ -63,8 +73,7 @@ class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView ):
         post = self.get_object()
         if self.request.user == post.author:
             return True
-        else:
-            False
+        return False
 
 
 def about(request):
